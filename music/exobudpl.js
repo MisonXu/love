0f181ed7f67aed3e546bd8cbefc817c1a389f7d4
xu Mison <xu Mison>
2016-02-15 00:21:24 +0800

    add xiaohua

---
+++ music/exobudpl.js
@@ -0,0 +1,2 @@
+mkList("http://61.134.97.252/trouble.mp3","trable is friend");
+mkList("http://bbs.gdut.cn/NGPXBBS/attach/bbscon/夏天的风.mp3B=Feeling&F=M.1252390541.A&attachpos=1461&attachname=/夏天的风.mp3","夏天的风-温岚");
